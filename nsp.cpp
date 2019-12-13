0a099234e55188cbf61a7ce6e23af97aa24e1711
KarthiKeyan <karthijob564@gmail.com>
2019-12-13 06:34:09 +0530

    initial commit

---
+++ nsp.cpp
@@ -0,0 +1,31 @@
+#include<string>
+#include<stdlib.h>
+#include<iostream>
+int main(int argn,char **arg){	std::string ar= arg[1];
+	std::string op[10];
+	std::string intap[50];
+	int r,j,e;
+	for(r=1,j=0,e=0;r<argn;r++){
+		if(arg[r][0]=='-') {op[j]=arg[r];j++;}
+		else {intap[e]=arg[r];e++;}
+	}
+	std::string a,c="g++ -o exec/"+ar+" bin/"+ar+".cpp";
+	for(j--;j>=0;j--){
+	c+=" "+op[j];
+	}
+	int re = system(c.c_str());
+	if ( re == 0 ){
+	 // c = "notify-send \"Build done\" \""+ar+" Build successful\"";      -previous implementation
+		std::cout<<"\n**************************************************************\n";
+		std::cout<<ar+" Build successful\n";
+		std::cout<<"**************************************************************\n\n^\n";
+		std::cout.flush();
+	    c = "./exec/"+ar;
+	    for(int i=1;i<e;i++){
+	        c += " "+intap[i];
+	    }
+		system(c.c_str());
+	}
+	else
+		exit(0);
+}
