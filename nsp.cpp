ee66348f6a6a11fb70f05ac7763c2f7252c4520e
KarthiKeyan <karthijob564@gmail.com>
2019-12-13 10:07:01 +0530

    minor directory changes ../

--- nsp.cpp
+++ nsp.cpp
@@ -9,7 +9,7 @@ int main(int argn,char **arg){	std::string ar= arg[1];
 		if(arg[r][0]=='-') {op[j]=arg[r];j++;}
 		else {intap[e]=arg[r];e++;}
 	}
-	std::string a,c="g++ -o exec/"+ar+" bin/"+ar+".cpp";
+	std::string a,c="g++ -o ../exec/"+ar+" ../bin/"+ar+".cpp";
 	for(j--;j>=0;j--){
 	c+=" "+op[j];
 	}
@@ -21,7 +21,7 @@ int main(int argn,char **arg){	std::string ar= arg[1];
 		std::cout<<ar+" Build successful\n";
 		std::cout<<"**************************************************************\n\n^\n";
 		std::cout.flush();
-	    c = "./exec/"+ar;
+	    c = "./../exec/"+ar;
 	    for(int i=1;i<e;i++){
 	        c += " "+intap[i];
 	    }
