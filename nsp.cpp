eefdda37002088b106a1997c0d096b4220bacf30
KarthiKeyan <karthijob564@gmail.com>
2020-03-25 13:21:14 +0530

    timing feature added

--- nsp.cpp
+++ nsp.cpp
@@ -1,6 +1,17 @@
 #include<string>
 #include<stdlib.h>
 #include<iostream>
+#include<chrono>
+void timer(std::string ar,std::string c)
+{
+	auto start = std::chrono::high_resolution_clock::now();
+	system(c.c_str());
+	auto stop = std::chrono::high_resolution_clock::now();
+	auto d= std::chrono::duration_cast<std::chrono::microseconds>(stop-start);
+	std::cout<<"\n**************************************************************\n";
+	std::cout<<ar+" Run successful" << "\t Time:"<<d.count()<<"us\n";
+	std::cout<<"**************************************************************\n\n^\n";
+}
 struct node{
 	std::string info;
 	node *next;
@@ -28,7 +39,7 @@ int main(int argn,char **arg){
 	stack<node,std::string> op;
 	stack<node,std::string> intap;
 	int r;
-	bool e=false;
+	bool e[2]={false,false};
 	std::string tem;
 	std::string ar = arg[1];
 	for(r=2;r<argn;r++){
@@ -36,7 +47,10 @@ int main(int argn,char **arg){
 		if(arg[r][0]=='-'){ 
 			if(arg[r][1]=='z'){
 				system("python3 create.py");
-				e=true;
+				e[0]=true;
+			}
+			if(arg[r][1]=='t'){
+				e[1]=true;
 			}
 			else
 			op.push(top1,tem);
@@ -51,19 +65,23 @@ int main(int argn,char **arg){
 	std::cout<<"\n**************************************************************\n";
 	int re = system(c.c_str());
 	if ( re == 0 ){
-	 // c = "notify-send \"Build done\" \""+ar+" Build successful\"";      -previous implementation
 		std::cout<<"\n**************************************************************\n";
 		std::cout<<ar+" Build successful\n";
-		std::cout<<"**************************************************************\n\n^\n";
+		std::cout<<"**************************************************************\n\n\n";
 		std::cout.flush();
 	    c = "./../exec/"+ar;
 	    while ( top2!=nullptr ){
 	        a =intap.pop(top2)+" "+a;
 	    }
 	    c+=" "+a;
-	    if(e==true) c+="<../.tem/temp.txt";
+	    if(e[0]==true)
+	    {
+	    c+="<../.tem/temp.txt";
 		system(c.c_str());
-		if(e==true) system("rm ../.tem/temp.txt");
+		system("rm ../.tem/temp.txt");
+		}
+		else if(e[1]==true) timer(ar,c);
+		else system(c.c_str());
 	}
 	else
 		exit(0);
